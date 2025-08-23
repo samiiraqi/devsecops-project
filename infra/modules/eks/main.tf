fc01f328fa09be6ad6ef36422822dab019910e39
samiiraqi <masdev7979@gmail.com>
2025-08-23 23:37:25 +0300

    ci: import existing nodegroup; eks: enable API auth mode

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -57,6 +57,11 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.cluster.arn
 
   version = var.kubernetes_version
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
 
   vpc_config {
     security_group_ids     = [aws_security_group.cluster.id]
